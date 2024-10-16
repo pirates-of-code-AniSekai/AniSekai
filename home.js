80c89075d069bd0e30137fdb3e63004bf41a6553
PrabhleenKaur28 <prabhleen544@gmail.com>
2024-10-16 20:44:46 +0530

    added files

---
+++ home.js
@@ -0,0 +1,57 @@
+document.addEventListener('scroll',()=>{
+  const nav=document.querySelector('.nav');
+  if(window.scrollY>0){
+    nav.classList.add('scrolled');
+  }else{
+    nav.classList.remove('scrolled');
+  }
+});
+
+const menuicons = document.querySelector('.menuicons');
+const openmenu = document.querySelector('.openmenu');
+
+openmenu.addEventListener("click", (event) => {
+  menuicons.classList.toggle('show');
+});
+
+let list=document.querySelector('.slider .list');
+let items=document.querySelectorAll('.slider .list .item');
+let dots=document.querySelectorAll('.slider .dots button');
+let prev=document.getElementById('prev');
+let next=document.getElementById('next');
+let active=0;
+let lengthItems=items.length-1;
+
+next.onclick = function(){
+  if(active+1>lengthItems){
+    active=0;
+  }else{
+  active+=1;
+  }
+  reloadSlider();
+}
+prev.onclick=function(){
+  if(active-1<0){
+    active=lengthItems;
+  }
+  else{
+    active=active-1;
+  }
+  reloadSlider();
+}
+let refreshSlider=setInterval(()=>{next.click()},5000);
+function reloadSlider(){
+  let checkLeft=items[active].offsetLeft;
+  list.style.left=-checkLeft+'px';
+  let lastActiveDot=document.querySelector('.slider .dots button.active');
+  lastActiveDot.classList.remove('active');
+  dots[active].classList.add('active');
+  clearInterval(refreshSlider);
+  refreshSlider=setInterval(()=>{next.click()},3000);
+}
+dots.forEach((button,key)=>{
+  button.addEventListener('click',function(){
+    active=key;
+    reloadSlider();
+  })
+});
---
+++ scriptani.js
@@ -0,0 +1,16 @@
+const openLogin = document.querySelector('.loginbutton');
+const login = document.querySelector('.login');
+const loginContent = document.querySelector('.loginbox');
+const cover = document.getElementById('cover');
+
+openLogin.addEventListener('click', () => {
+  const isOpen = loginContent.classList.toggle('open');
+  if (isOpen) {
+    login.style.display = 'flex';
+    cover.style.display = 'flex';
+    openLogin.style.zIndex = 5;
+  } else {
+    login.style.display = 'none';
+    cover.style.display = 'none';
+  }
+});
