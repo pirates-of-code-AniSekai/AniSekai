f1879959ff8924cff44863bc38d1ab2022cb8960
rehan <rehanchalanaprsnl@gmail.com>
2024-10-17 22:17:57 +0530

    feat: added api-support
    - for Movies
    - for Most Popular
    - for Specials

--- apiscript.js
+++ apiscript.js
@@ -1,5 +1,8 @@
 const baseUrl = "https://anime-api-steel.vercel.app"
 const trendingGridDiv = document.querySelector(".trendingList");
+const popularListDiv = document.querySelector(".popularList");
+const specialListDiv = document.querySelector(".specialList");
+const movieListDiv = document.querySelector(".movieList");
 
 async function getTrending() {
     try {
@@ -19,12 +22,10 @@ async function getTrending() {
                                 </div>
                             </div>
                         `;
-            console.log(data)
             let child = document.createElement("li");
             child.innerHTML = childHtml
             trendingGridDiv.appendChild(child);
         })
-        console.log(json.results)
     } catch (error) {
         console.error(error)
     }
@@ -33,8 +34,38 @@ async function getTrending() {
 
 async function getPopular() {
     try {
-        const response = await fetch(`${baseUrl}/meta/anilist/trending`);
+        const response = await fetch(`${baseUrl}/meta/anilist/popular`);
         const json = await response.json();
+        json.results.map(data => {
+            let childHtml = `<img src="${data.image}">
+                            <div class="aboutanime" style="text-align:justify;">
+                                <h2>${data.title.english}</h2><br>
+                                <p>${data.description}</p>
+                                <div class="aboutMenu"
+                                    style="display: flex; justify-content: space-evenly; position: relative;top: 1vh;">
+                                    <div title="LIKE" class="aboutmenu"><img src="Icons/heart.png"></div>
+                                    <div class="aboutmenu"><img title="WISHLIST" src="Icons/wishlist.png"></div>
+                                    <div class="aboutmenu"><img title="SHARE" src="Icons/paper-plane.png"></div>
+                                    <div class="aboutmenu"><img title="NOTIFICATIONS" src="Icons/bell.png"></div>
+                                </div>
+                            </div>
+                        `;
+        
+            let child = document.createElement("li");
+            child.innerHTML = childHtml
+            popularListDiv.appendChild(child);
+        })
+        
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+async function getSpecial() {
+    try {
+        const response = await fetch(`${baseUrl}/meta/anilist/advanced-search?format=SPECIAL`);
+        const json = await response.json();
+        console.log(json)
         json.results.map(data => {
             let childHtml = `<img src="${data.image}">
                             <div class="aboutanime" style="text-align:justify;">
@@ -52,7 +83,7 @@ async function getPopular() {
             console.log(data)
             let child = document.createElement("li");
             child.innerHTML = childHtml
-            trendingGridDiv.appendChild(child);
+            specialListDiv.appendChild(child);
         })
         console.log(json.results)
     } catch (error) {
@@ -60,6 +91,40 @@ async function getPopular() {
     }
 }
 
+async function getMovies() {
+    try {
+        const response = await fetch(`${baseUrl}/meta/anilist/advanced-search?format=MOVIE`);
+        const json = await response.json();
+        console.log(json)
+        json.results.map(data => {
+            let childHtml = `<img src="${data.image}">
+                            <div class="aboutanime" style="text-align:justify;">
+                                <h2>${data.title.english}</h2><br>
+                                <p>${data.description}</p>
+                                <div class="aboutMenu"
+                                    style="display: flex; justify-content: space-evenly; position: relative;top: 1vh;">
+                                    <div title="LIKE" class="aboutmenu"><img src="Icons/heart.png"></div>
+                                    <div class="aboutmenu"><img title="WISHLIST" src="Icons/wishlist.png"></div>
+                                    <div class="aboutmenu"><img title="SHARE" src="Icons/paper-plane.png"></div>
+                                    <div class="aboutmenu"><img title="NOTIFICATIONS" src="Icons/bell.png"></div>
+                                </div>
+                            </div>
+                        `;
+            console.log(data)
+            let child = document.createElement("li");
+            child.innerHTML = childHtml
+            movieListDiv.appendChild(child);
+        })
+        console.log(json.results)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+
 getTrending()
+getPopular()
+getSpecial()
+getMovies()
 
 
