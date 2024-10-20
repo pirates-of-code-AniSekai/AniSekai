9c13a17c38f9a1592c4b1a5404a4aa999f74759a
rehan <rehanchalanaprsnl@gmail.com>
2024-10-20 21:49:05 +0530

    feat: added recommendation on content page

--- src/main/resources/static/content.js
+++ src/main/resources/static/content.js
@@ -1,5 +1,4 @@
 
-
 const anime_id = localStorage.getItem("anime_id");
 const baseUrl = "https://anime-api-steel.vercel.app/meta/anilist/info"
 
@@ -7,6 +6,9 @@ const bg_cover = document.querySelector(".bg-cover");
 const poster = document.querySelector(".poster");
 const title = document.querySelector(".title");
 const desc = document.querySelector(".description");
+const episodeList = document.querySelector(".episodes");
+const recList = document.querySelector(".recList");
+const yearDiv = document.querySelector(".year");
 
 const loadData = async () => {
     const response = await fetch(`${baseUrl}/${anime_id}`);
@@ -17,8 +19,31 @@ const loadData = async () => {
     poster.src = json.image;
     title.innerHTML = json.title.english;
     desc.innerHTML = json.description;
+    yearDiv.innerHTML = json.releaseDate;
+
+    json.episodes.map((ep) => {
+        let child = document.createElement("div");
+        child.classList.add("episode");
+        child.innerHTML = `<img src="Icons/play-button.png">${ep.title}`;
+        child.addEventListener("click",() => {
+            localStorage.setItem("episode_id",ep.id);
+            window.location.href = "player.html";
+        })
+        episodeList.appendChild(child);
+    })
 
 
+    json.recommendations.map((anime) => {
+        let child = document.createElement("li");
+        child.innerHTML = `<img src=${anime.image}>`;
+            child.addEventListener("click",() => {
+                localStorage.clear();
+                localStorage.setItem("anime_id",anime.id);
+
+                window.location.href = "content.html";
+            })
+        recList.appendChild(child);
+    })
 }
 
 
