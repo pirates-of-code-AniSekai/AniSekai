beb571931e5a11fa14502faf36b4845a252af77b
rehan <rehanchalanaprsnl@gmail.com>
2024-10-20 22:22:54 +0530

    feat: add functionality for player

---
+++ src/main/resources/static/player.js
@@ -0,0 +1,22 @@
+const episode_id = localStorage.getItem("episode_id");
+const baseUrl = "https://anime-api-steel.vercel.app/meta/anilist/watch";
+
+let hls = new Hls();
+const video = document.getElementById('video');
+
+
+async function loadVideo() {
+    const response = await fetch(`${baseUrl}/${episode_id}`);
+    const json = await response.json();
+    console.log(json);
+    
+    let source720p = json.sources.find(source => source.quality === "720p");
+    hls.loadSource(source720p.url);
+    hls.attachMedia(video);
+}
+
+loadVideo();
+
+
+
+
