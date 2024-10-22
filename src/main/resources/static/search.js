1a241baa2ebc95fa4368fd31efdd95604c993b0d
rehan <rehanchalanaprsnl@gmail.com>
2024-10-22 09:27:12 +0530

    feat: implement search page functionality

--- src/main/resources/static/search.js
+++ src/main/resources/static/search.js
@@ -2,15 +2,35 @@ const searchInput = document.querySelector("#input");
 const searchBtn = document.querySelector(".searchBtn");
 const baseUrl = "https://anime-api-steel.vercel.app/meta/anilist"
 
+const searchResults = document.querySelector(".search-results");
+const searchResultsHeader = document.querySelector(".search-result-for");
+
 searchBtn.addEventListener("click",() => {
     let inp = searchInput.value;
     console.log(inp);
+
+    if(!inp) searchResultsHeader.innerHTML = "<h1>please search something first</h1>";
+    else searchResultsHeader.innerHTML =   `<h1>showing search results for : ${inp}</h1>`;
+    searchResults.innerHTML = "";
     getData(inp);
 })
 
 async function getData(search) {
     const response = await fetch(`${baseUrl}/${search}`);
     const json = await response.json();
+
+    json.results.map((anime) => {
+        let anDiv = document.createElement("div");
+        anDiv.classList.add("search-res-box");
+        anDiv.innerHTML = `<img src=${anime.image}>`;
+        anDiv.addEventListener("click", () => {
+            localStorage.clear();
+            localStorage.setItem("anime_id",anime.id);
+            window.location.href = "content.html";
+        })
+        searchResults.appendChild(anDiv);
+    })
+    
 
     console.log(json);
 }
