b4c7fe73e34cdb42ced66d166a79d183c4d75e3b
rehan <rehanchalanaprsnl@gmail.com>
2024-10-22 19:13:25 +0530

    fix: fixed video quality issue

--- src/main/resources/static/player.js
+++ src/main/resources/static/player.js
@@ -1,4 +1,5 @@
 const episode_id = localStorage.getItem("episode_id");
+console.log(episode_id);
 const baseUrl = "https://anime-api-steel.vercel.app/meta/anilist/watch";
 
 let hls = new Hls();
@@ -10,8 +11,18 @@ async function loadVideo() {
     const json = await response.json();
     console.log(json);
     
-    let source720p = json.sources.find(source => source.quality === "1080p");
-    hls.loadSource(source720p.url);
+    let source = json.sources.find(source => source.quality === "1080p");
+    if(!source) {
+        console.log("first")
+        source = json.sources.find(source => source.quality === "720p");
+    } if(!source) {
+        console.log("second")
+        source = json.sources.find(source => source.quality === "480p");
+    } if(!source) {
+        source = json.sources.find(source => source.quality === "default");
+    }
+    console.log(source)
+    hls.loadSource(source.url);
     hls.attachMedia(video);
 }
 
