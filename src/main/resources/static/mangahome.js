3206ec8436d20755bf3343e2583e096911193725
PrabhleenKaur28 <prabhleen544@gmail.com>
2024-11-11 14:54:17 +0530

    manga home pg

---
+++ src/main/resources/static/mangahome.js
@@ -0,0 +1,65 @@
+document.addEventListener('scroll',()=>{
+    const nav=document.querySelector('.nav');
+    if(window.scrollY>0){
+      nav.classList.add('scrolled');
+    }else{
+      nav.classList.remove('scrolled');
+    }
+  });
+  
+  const menuicons = document.querySelector('.menuicons');
+  const openmenu = document.querySelector('.openmenu');
+  
+  openmenu.addEventListener("click", (event) => {
+    menuicons.classList.toggle('show');
+    openmenu.classList.toggle('show');
+  });
+  
+  const profilebtn=document.querySelector('.profile-icon');
+  const profile=document.querySelector('.profile-bar');
+  profilebtn.addEventListener("click", (event) => {
+    profile.classList.toggle('open');
+  });
+
+  let list=document.querySelector('.slider .list');
+  let items=document.querySelectorAll('.slider .list .item');
+  let dots=document.querySelectorAll('.slider .dots button');
+  let prev=document.getElementById('prev');
+  let next=document.getElementById('next');
+  let active=0;
+  let lengthItems=items.length-1;
+  
+  next.onclick = function(){
+    if(active+1>lengthItems){
+      active=0;
+    }else{
+    active+=1;
+    }
+    reloadSlider();
+  }
+  prev.onclick=function(){
+    if(active-1<0){
+      active=lengthItems;
+    }
+    else{
+      active=active-1;
+    }
+    reloadSlider();
+  }
+  let refreshSlider=setInterval(()=>{next.click()},5000);
+  function reloadSlider(){
+    let checkLeft=items[active].offsetLeft;
+    list.style.left=-checkLeft+'px';
+    let lastActiveDot=document.querySelector('.slider .dots button.active');
+    lastActiveDot.classList.remove('active');
+    dots[active].classList.add('active');
+    clearInterval(refreshSlider);
+    refreshSlider=setInterval(()=>{next.click()},3000);
+  }
+  dots.forEach((button,key)=>{
+    button.addEventListener('click',function(){
+      active=key;
+      reloadSlider();
+    })
+  });
+  
