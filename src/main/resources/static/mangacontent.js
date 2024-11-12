3fae5830b8786e2e7a9fba59dc4103989a45e9fb
PrabhleenKaur28 <prabhleen544@gmail.com>
2024-11-12 12:58:09 +0530

    Manga pgs next and prev btn working

--- src/main/resources/static/mangacontent.js
+++ src/main/resources/static/mangacontent.js
@@ -6,3 +6,26 @@ document.getElementById('fullScreenBtn').addEventListener('click', function() {
 });
 
 console.log(localStorage.getItem('episode_id'));
+
+const images = ['Icons/final one piece.jpg',
+    'Icons/final jjk.jpg', 
+    'Icons/final dragon ball.jpg',
+    'Icons/final death note.jpg'
+];
+let currentIndex = 0;
+const image = document.getElementById('image');
+const prev= document.getElementById('prev');
+const next = document.getElementById('next');
+
+function updateImage(){
+    image.src = images[currentIndex];
+}
+prev.addEventListener('click', ()=>{
+    currentIndex = (currentIndex>0)? currentIndex-1 : images.length-1;
+    updateImage();
+});
+next.addEventListener('click', ()=>{
+    currentIndex = (currentIndex<images.length-1)? currentIndex+1 : 0;
+    updateImage();
+});
+updateImage();
